6c29569d961d1e7a181229dd4c6e669aa7c5630c
miwa nakano <miwako102030@gmail.com>
2026-09-05 20:33:36 +0900

    fastcommit

---
+++ wp-content/themes/wp-exam/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>WordPressテーマ化練習用サイト | TOP</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+  <!-- ヘッダーエリア -->
+  <header class="site-header">
+    <div class="container">
+      <div class="site-logo">
+        <a href="index.html">WP Practice</a>
+      </div>
+      <nav class="global-nav">
+        <ul>
+          <li><a href="index.html">ホーム</a></li>
+          <li><a href="page.html">私たちについて</a></li>
+          <li><a href="archive.html">ブログ</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
